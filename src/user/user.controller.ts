9470e8d257bac5a6a124223e5de77e2ffc653746
Mehmet Günhan <mgunhan736@gmail.com>
2026-05-02 19:23:17 +0300

    Kullanıcı kayıdını alma işlemini kurduk.Kayıt Sistemi ile pgadmin4 uygulamasında Postsql ile tablomuzu oluşturduk.Kayıt olan kişileri tabloya aktarılması ve ana menüye geçişi için kayıtan kullanıcı verisi çekmesini sağladım.

---
+++ src/user/user.controller.ts
@@ -0,0 +1,16 @@
+import { Controller, Post, Body } from '@nestjs/common';
+import { UserService } from './user.service';
+
+@Controller('auth') // Burası 'auth' olmalı
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post('register') // Burası 'register' olmalı
+  register(@Body() body: any) {
+    return this.userService.register(body);
+  }
+  @Post('login')
+async login(@Body() body: any) {
+  return this.userService.login(body.username, body.password);
+}
+}
